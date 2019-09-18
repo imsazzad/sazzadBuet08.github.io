cfafd7661c69a0f2572f0e741c91742818baa513
sazzad <abdul.hasib@infolytx.com>
2019-09-18 22:41:12 +0600

    added source code

---
+++ src/resumeData.js
@@ -0,0 +1,132 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Md Abdul Hasib",
+  "role": "Senior Software Engineer| Machine Learning Practitioner| CSM| Problem Solver| Big Data Enthusiast| Project Lead",
+  "linkedinId":"https://www.linkedin.com/in/md-abdul-hasib-sazzad-19b88099/",
+  "skypeid": "abdulhasibsazzad@hotmail.com",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/md-abdul-hasib-sazzad-19b88099/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/sazzadBuet08",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"live:abdulhasibsazzad",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"He would like to introduce himself as a machine learning engineer(especially deep learning) and backend engineer specialized in python and java. He possesses excellent verbal and written communication skills. He is a quick learner." ,
+  "address":"Bangladesh",
+  "website":"https://www.linkedin.com/in/md-abdul-hasib-sazzad-19b88099/",
+  "education":[
+    {
+      "UniversityName":"BUET",
+      "specialization":"Computer Science & Engineering",
+      "MonthOfPassing":"Aug",
+      "YearOfPassing":"2014",
+      "Achievements":"Some Achievements"
+    },
+  ],
+  "work":[
+    {
+      "CompanyName":"Infolytx Inc",
+      "specialization":"Senior Software Engineer",
+      "MonthOfLeaving":"from",
+      "YearOfLeaving":"Nov 2016",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Infolytx Inc",
+      "specialization":"Software Engineer",
+      "MonthOfLeaving":"from",
+      "YearOfLeaving":"Sep 2015",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Nascenia",
+      "specialization":"Juniro Software Engineer",
+      "MonthOfLeaving":"from",
+      "YearOfLeaving":"Aug 2014",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"machine learning"
+    },
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "top-skills":[
+    {
+      "skillname":"Machine Learning"
+    },
+    {
+      "skillname":"Deep Learning"
+    },
+    {
+      "skillname":"JAVA"
+    },
+    {
+      "skillname":"Reactjs"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"DeepCare: Activity Recognition via Deep Learning",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"Sazzad and I worked together at Infolytx. He is a young developer with good coding skills and technical knowledge under his belt. He is grooming himself up with a diversified set of software development skills and has a lot of potential. He is very receptive to feedback and takes a great interest in doing things the right way. A very adaptive person Sazzad has the courage to pick up work on areas that he hasn’t worked in before and his enthusiasm is very impressive. I had an enjoyable experience with Sazzad and wish to see him contributing to the community as a great software developer going forward.",
+      "name":"Rezwanul Huda",
+      "role" : "SOFTWARE CRAFTSMAN | AGILE EVANGELIST | COACH | ARCHITECT"
+    },
+    {
+      "description":"The very first thing that I can say about Sazzad is that he is a very enthusiastic learner. Back in 2011 we were both part of a Software Engineering study group, and even though he was still a student and had tons of coursework he still managed to attend all the regular study sessions. This also shows that he is a hard worker. He has very good knowledge of OOP and basic Software Engineering as well. As a person he is very humble, modest, and helpful. I wish him all the best in his career!",
+      "name":"MD Sayem Ahmed",
+      "role" : "Senior Software Engineer at ImmobilienScout24\n" +
+          "Berlin, Berlin, Germany"
+    }
+  ],
+
+  "project_details":{
+    "deep_care":"The DeepCare solution allows Certified Nursing Assistants (CNAs) and nurses to monitor more closely the ambulatory status of residents at eldercare and assisted living facilities. DeepCare uses Deep Learning and other machine learning techniques to determine whether a resident is sitting, walking, standing or climbing stairs. Wearables or smartphones outfitted with the app continuously sends sensor (accelerometer, gyroscope, GPS) data to a back-end server. The system then uses the machine-learning models to accurately predict the class of activity the user is performing.\n" +
+        "\n" +
+        "The system then continuously sends status notifications to caregivers via both a desktop web application and a mobile app. When the system detects adverse (e.g. such as falling) or unusual events (such as no motion for a long time during the day) alerts are sent both to companion apps worn by caregivers and a web application that might be installed at a nurse’s station"
+  }
+}
+
+export default resumeData
